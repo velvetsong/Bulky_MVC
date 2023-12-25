5ac54f213937ceea89fd6131361f193245e2672f
Peyton Boggia <peytonboggia@gmail.com>
2023-12-25 16:58:00 -0500

    Changed the company.js file  to include postalCode

--- BulkyBookWeb/wwwroot/js/company2.js
+++ BulkyBookWeb/wwwroot/js/company2.js
@@ -12,6 +12,7 @@ function loadDataTable() {
             { "data": "streetAddress", "width": "15%" },
             { "data": "city", "width": "15%" },
             { "data": "state", "width": "15%" },
+            { "data": "postalCode", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
             {
                 data: 'id',
