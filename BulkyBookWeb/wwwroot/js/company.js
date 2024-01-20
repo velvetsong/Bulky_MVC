5ab6c3ae361ba3a805b1b489f017566d0a11a2a1
Peyton Boggia <peytonboggia@gmail.com>
2024-01-19 23:51:39 -0500

    more company.js  edits

--- BulkyBookWeb/wwwroot/js/company.js
+++ BulkyBookWeb/wwwroot/js/company.js
@@ -11,7 +11,7 @@ function loadDataTable() {
         },
         "columns": [
             { "data": "name", "width": "15%" },
-            { "data": "address", "width": "15%" },
+            { "data": "streetAddress", "width": "15%" },
             { "data": "city", "width": "15%" },
             { "data": "state", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
