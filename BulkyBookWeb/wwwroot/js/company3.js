d4b81352d72355690ed21b77df0ba27b69db08aa
Peyton Boggia <peytonboggia@gmail.com>
2024-01-19 21:47:06 -0500

    format company3.js

--- BulkyBookWeb/wwwroot/js/company3.js
+++ BulkyBookWeb/wwwroot/js/company3.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url":"/Company/GetAll"
+            "url": "/Company/GetAll"
         },
         "columns": [
             { "data": "name", "width": "15%" },
@@ -16,12 +16,14 @@ function loadDataTable() {
             { "data": "state", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
             {
-                "data": 'id', 
+                "data": "id",
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
-                     <a href="/company/createupdate?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a onClick=Delete('/company/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
-                    </div>`
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                        <a href="/company/createupdate?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                        <a onClick=Delete('/company/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>
+                         `
                 },
                 "width": "15%"
             }
